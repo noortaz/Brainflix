474e7cccfe20490fb61fc8a9e235e867ff7549cb
noortaz <noortazc@gmail.com>
2019-07-25 16:41:07 -0400

    header and hero in progress

--- sprint-1/src/App.js
+++ sprint-1/src/App.js
@@ -1,25 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles.scss';
+import headerLogo  from './logo/logo-brainflix.svg';
+import avatar from './images/avatar.jpg';
+import searchIcon from './icons/SVG/Icon-search.svg';
+import uploadIcon from './icons/SVG/Icon-upload.svg';
+import playIcon from './icons/SVG/Icon-play.svg';
+import volumeIcon from './icons/SVG/Icon-volume.svg';
+import fullScreen from './icons/SVG/Icon-fullscreen.svg';
+import heroImage from './images/video-list-0.jpg';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <nav className="nav-bar">
+          <ul className="nav-bar__list">
+            <li className="nav-bar__list__items">
+              
+              <img className="nav-bar__logo" src={headerLogo} alt=""/>
+              
+            </li>
+            <li className="nav-bar__list__items">
+              <img className="nav-bar__icon--search" src={searchIcon} alt=""/>
+              <input className="nav-bar__search-box" type="name" placeholder="Search"/>
+            </li>
+            <li className="nav-bar__list__items">
+              <button className="nav-bar__upload-btn">
+                <img className="nav-bar__icon--upload" src={uploadIcon} alt=""/>
+                 upload
+              </button>
+              <img className="nav-bar__avatar" src={avatar} alt=""/>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
+}
+
+
+class Hero extends React.Component {
+  render() {
+    return (
+      <section className="hero-container">
+          <img className= "hero-container__image" src={heroImage} alt=""/>
+          <div className= "hero-container__icons">
+            <img className="hero-container__icons--play" src={playIcon} alt=""/>
+            <img className="hero-container__icons--" src={fullScreen} alt=""/>
+            <img className="hero-container__icons--" src={volumeIcon} alt=""/>
+          </div>
+      </section>
+    )
+  }
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Hero />
+    </>
   );
 }
 

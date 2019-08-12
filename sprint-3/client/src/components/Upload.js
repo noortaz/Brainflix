1578e678de3465b4292ef9332f4abf883a788e87
noortaz <noortazc@gmail.com>
2019-08-11 21:28:37 -0400

    upload functionality added

--- sprint-3/client/src/components/Upload.js
+++ sprint-3/client/src/components/Upload.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import uploadPreview from '../images/upload-preview.jpg';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Axios from 'axios';
 
 
@@ -10,32 +10,31 @@ class Upload extends React.Component {
     title: '',
     channel: 'Video Uploader',
     description: '',
-    image: "https://i.imgur.com/q3lL1cE.jpg"
+    image: uploadPreview
   }
 
-  
+
   postVideoList = () => {
-  Axios.post('http://localhost:5000/videos', {
-    title: this.state.title,
-    channel: this.state.channel,
-    image: "https://i.imgur.com/q3lL1cE.jpg",
-    description: this.state.description
-  })
-    .then(response => {
-      console.log(response);
+    Axios.post('http://localhost:5000/videos', {
+      title: this.state.title,
+      channel: this.state.channel,
+      image: this.state.image,
+      description: this.state.description
     })
+      .then(response => {
+        console.log(response);
+      })
   }
 
 
   submitHandler = (event) => {
     event.preventDefault();
     this.postVideoList();
-    
+
   }
 
   uploadTitle = (event) => {
     const newTitle = event.target.value;
-    const newDes = event.target.value;
     this.setState({
       title: newTitle,
     })
@@ -58,38 +57,31 @@ class Upload extends React.Component {
   render() {
 
     return (
-
-          <form action="" className="upload" onSubmit={this.submitHandler}>
-            <h2 className="upload__heading">Upload video</h2>
-            <div className="upload__container">
-              <div className="upload__thumbnail">
-                <label className="upload__content__label">Video Thumbnail</label>
-                <img className="upload__content__image" src={this.state.image} alt=""/>
-              </div>             
-              <div className="upload__content">
-                <div className="upload__content--message">
-                  <label className="upload__content__label">title your video</label>
-
-                  <textarea onChange={this.uploadTitle} className="upload__content__input" name="title"
+      <form action="" className="upload" onSubmit={this.submitHandler}>
+        <h2 className="upload__heading">Upload video</h2>
+        <div className="upload__container">
+          <div className="upload__thumbnail">
+            <label className="upload__content__label">Video Thumbnail</label>
+            <img className="upload__content__image" src={this.state.image} alt="" />
+          </div>
+          <div className="upload__content">
+            <div className="upload__content--message">
+              <label className="upload__content__label">title your video</label>
+              <textarea onChange={this.uploadTitle} className="upload__content__input" name="title"
                 placeholder="Add a title to your video" ></textarea>
-
-                <p>{this.state.title}</p>
-                </div>
-                <div className="upload__content--message">
-                  <label className="upload__content__label">Add a video description</label>
-
+            </div>
+            <div className="upload__content--message">
+              <label className="upload__content__label">Add a video description</label>
               <textarea onChange={this.uploadDescription} className="upload__content__input" name="description" rows="8" cols="45"
                 placeholder="Add a description to your video" ></textarea>
-              <p>{this.state.description}</p>
-
-                </div>  
-              </div>
             </div>
-            <div className="upload--submission">
-              <button className="button-publish" method="POST" type="submit">Publish</button>
-              <Link to="/upload" className="upload--cancel">CANCEL</Link>
-            </div>
-          </form>
+          </div>
+        </div>
+        <div className="upload--submission">
+          <button className="button-publish" method="POST" type="submit">Publish</button>
+          <Link to="/upload" className="upload--cancel">CANCEL</Link>
+        </div>
+      </form>
     )
   }
 }
